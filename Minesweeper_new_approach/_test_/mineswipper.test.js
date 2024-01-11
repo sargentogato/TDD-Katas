bfd6700a203d02d69aab9dbb01f9170df9f3d481
Marlon Velasquez <marlonjvelasquez@gmail.com>
2024-01-11 12:15:45 +0100

    Add Minesweeper new approachº:wq

---
+++ Minesweeper_new_approach/_test_/mineswipper.test.js
@@ -0,0 +1,14 @@
+const Mineswipper = require("../minesweeper");
+
+const Game = new Mineswipper();
+
+describe("Mineswipper", () => {
+  test("test enviroment", () => {
+    //Arrange
+
+    //Action
+    //Assert
+
+    expect(true).toBe(true);
+  });
+});
---
+++ Minesweeper_new_approach/minesweeper.js
@@ -0,0 +1,3 @@
+class Mineswipper {}
+
+module.exports = Mineswipper;
